af04798a3cf7e00308bfc09e2a3f1ba18807ca6f
Cleibson Almeida <nosbielcs@bol.com.br>
2017-09-19 02:34:12 -0300

    regression last example

--- script/04_regression_model.R
+++ script/04_regression_model.R
@@ -16,6 +16,8 @@ model$coefficients
 model$coefficients[1] #intersection in y axys
 model$coefficients[2] #inclination
 summary(model)
+plot(speed ~ dist, data = cars)
+abline(model)
 
 #predict for dist = 25
 model$coefficients[1] + model$coefficients[2] * 25
@@ -25,3 +27,11 @@ predict(model, data.frame(dist=25))
 #validation model
 ##residuals
 model$residuals
+##fitted values
+model$fitted.values
+plot(model$fitted.values) #fitted values
+abline(model)
+##validation graphs
+par(mfrow = c(2, 2))
+plot(model)
+par(mfrow = c(1, 1))
