2ff39f581ede8a4d91814d77b31c127322c88623
Cleibson Almeida <nosbielcs@bol.com.br>
2017-09-22 01:35:36 -0300

    r_basic

---
+++ script/00_r_basic.R
@@ -0,0 +1,13 @@
+
+
+#Vetores
+
+##atribuindo valores
+idade <- c(23,34,56)
+##atribuindo valores com scan
+peso <- scan()
+##tamanho do vetor
+length(idade)
+##atribuindo string no vetor
+nomes <- c("Jorge", "Pedro", "Marcos")
+length(nomes)
